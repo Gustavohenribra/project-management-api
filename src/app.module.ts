6d312da63b342589b329bec26f069af35a08fedb
GustavoHenriBra <gustavohenriquebm7@outlook.com>
2025-01-07 17:38:19 -0300

    chore: add modules

---
+++ src/app.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { ProjectsModule } from './projects/projects.module';
+import { PdfService } from './common/pdf.service';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'database.sqlite',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    AuthModule,
+    UsersModule,
+    ProjectsModule,
+  ],
+  providers: [PdfService],
+  exports: [PdfService],
+})
+export class AppModule {}
